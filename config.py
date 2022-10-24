6bf26217d88504bd66d8b86efefe08a7fea3292c
QobiljonProweb <qobiljonm1993@gmail.com>
2022-10-24 18:34:37 +0500

    first commit

---
+++ config.py
@@ -0,0 +1,15 @@
+"""
+BU YERDA SIZGA KERAKLI BO'LADIGAN TOKEN VA SHUNGA O'XSHAGAN MA'LUMOTLAR SAQLANADI
+"""
+TOKEN = '5455162263:AAFvH0QHfnLo5NAIjXfcusJlg5u0Tf9tNkg'
+
+PROVIDER_TOKEN = "371317599:TEST:1666273308151"
+ADMINS = '''Adminlarni saqlash uchun o'zgaruvchi, listdaham saqlasa bo'ladi'''
+
+DB_NAME = '''dvnci668sc862'''
+DB_HOST = '''ec2-99-81-16-126.eu-west-1.compute.amazonaws.com'''
+DB_PASSWORD = '''1e250c905dd96e69da2d8627b32a8cecfdbe1612081351622acc117f1229636d'''
+DB_PORT = '''5432'''
+DB_USER = '''mcsopjlhbztysg'''
+
+
